27b34fd95835afa7282edbfba9fbe46a7af14aaa
hw <hewei@powerinfo.net>
2024-09-27 18:30:12 +0800

    添加视频转换函数；

--- main.py
+++ main.py
@@ -1,11 +1,4 @@
 # -*- coding: utf-8 -*-
-
-# Form implementation generated from reading ui file 'connect_me.ui'
-#
-# Created by: PyQt5 UI code generator 5.11.3
-#
-# WARNING! All changes made in this file will be lost!
-# 导入程序运行必须模块
 import os
 import sys
 
@@ -13,7 +6,7 @@ from PySide6.QtWidgets import QApplication, QMainWindow, QFileDialog
 
 # 导入designer工具生成的login模块
 from VideoConversion_ui import Ui_VideoConversionWindow
-from utils.ffmpeg_handler import get_video_info
+from utils.ffmpeg_handler import convert_video, get_video_info
 
 VIDEO_FORMATS = ['avi', 'flv', 'mp4', 'mkv']
 VIDEO_ENCODINGS = ['h264', 'h265']
@@ -24,23 +17,17 @@ class MainForm(QMainWindow, Ui_VideoConversionWindow):
         super().__init__(parent)
         self.setupUi(self)
 
-        self.select_video_btn.clicked.connect(self.show_file_dialog)
-        self.ecode_comb_box.addItems(VIDEO_ENCODINGS)
+        self.select_video_btn.clicked.connect(self.select_source_video_dialog)
+        self.encode_comb_box.addItems(VIDEO_ENCODINGS)
         self.format_comb_box.addItems(VIDEO_FORMATS)
-        self.out_video_btn.clicked.connect(self.show_dir_dialog)
+        self.out_video_btn.clicked.connect(self.select_out_video_dir_dialog)
         self.conversion_video_btn.clicked.connect(self.conversion_video)
 
         self.source_video_path = None
         self.out_video_dir = None
         self.out_video_path = None
 
-    def accept(self):
-        print('确定')
-
-    def reject(self):
-        print('取消')
-
-    def show_file_dialog(self):
+    def select_source_video_dialog(self):
         video_formats = f'视频类型 ({" ".join("*." + _ for _ in VIDEO_FORMATS)})'
         file_path, _ = QFileDialog.getOpenFileName(
             self, '选择视频', filter=video_formats)
@@ -52,8 +39,9 @@ class MainForm(QMainWindow, Ui_VideoConversionWindow):
                 video_name = os.path.basename(file_path)
                 video_info_text = f'{video_name}: ' + ', '.join(video_info)
                 self.video_info_label.setText(video_info_text)
+                # self.format_comb_box.setCurrentText(video_info[0])
 
-    def show_dir_dialog(self):
+    def select_out_video_dir_dialog(self):
         dir_path = QFileDialog.getExistingDirectory(
             self, '选择输出目录', './out_videos')
         if dir_path:
@@ -65,9 +53,12 @@ class MainForm(QMainWindow, Ui_VideoConversionWindow):
         if self.source_video_path and self.out_video_dir:
             self.out_video_path = os.path.join(
                 self.out_video_dir,
-                f'{os.path.splitext(os.path.basename(self.source_video_path))[0]}.mp4'
+                f'{os.path.splitext(os.path.basename(self.source_video_path))[0]}.{self.format_comb_box.currentText()}'
             )
             print(self.out_video_path)
+            convert_video(self.source_video_path, self.out_video_path,
+                          out_video_encode=self.encode_comb_box.currentText(),
+                          out_video_format=self.format_comb_box.currentText())
 
 
 if __name__ == "__main__":
--- utils/ffmpeg_handler.py
+++ utils/ffmpeg_handler.py
@@ -23,12 +23,51 @@ def get_video_info(video_path: Union[str, Path]) -> tuple:
     p = subprocess.Popen([FFMPEG_EXE, '-i', video_path, '-hide_banner'],
                          stdout=subprocess.PIPE, stderr=subprocess.PIPE)
     video_info_str = p.stderr.read().decode(ENCODING)
-    print('err:', video_info_str)
-    res = re.match(r'.+Duration: ([\d:\.]+), start.+bitrate: (\d+) kb/s.+Video: (.+?) .+, (\d+?)x(\d+?) .+', video_info_str, flags=re.DOTALL)
+    print(video_info_str)
+    video_info_re = (r'.+Duration: ([\d:\.]+), start.+bitrate: (\d+) kb/s.+'
+                     r'Video: (.+?) .+, (\d+?)x(\d+?) .+')
+    res = re.match(video_info_re, video_info_str, flags=re.DOTALL)
     if res:
-        print(res.groups())
         return res.groups()
     return None
+
+
+def convert_video(source_video_path: Union[str, Path], out_video_path: Union[str, Path],
+                  source_video_encode: str = None, out_video_encode: str = None,
+                  out_video_format: str = None, out_video_bitrate: str = None,
+                  out_video_fps: str = None, out_video_res: str = None):
+    """
+    视频转换
+
+    :params source_video_path: 源视频路径
+    :params out_video_path: 输出视频路径
+    :params source_video_encode: 源视频编码
+    :params out_video_encode: 输出视频编码
+    :params out_video_format: 输出视频格式
+    :params out_video_bitrate: 输出视频码率
+    :params out_video_fps: 输出视频帧率
+    :params out_video_res: 输出视频分辨率
+    """
+    cmd = [FFMPEG_EXE, '-c:v', source_video_encode,
+           '-i', source_video_path, '-hide_banner']
+    if out_video_encode:
+        cmd.append('-c:v')
+        cmd.append(out_video_encode)
+    if out_video_format:
+        cmd.append('-f')
+        cmd.append(out_video_format)
+    if out_video_bitrate:
+        cmd.append('-b:v')
+        cmd.append(out_video_bitrate)
+    if out_video_fps:
+        cmd.append('-r')
+        cmd.append(out_video_fps)
+    if out_video_res:
+        cmd.append('-s')
+        cmd.append(out_video_res)
+    cmd.append(out_video_path)
+    print(' '.join(cmd))
+    # p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
 
 if __name__ == '__main__':
